508c9ab8de967512f45f86b2eb9e7be54af80aa0
lheesoo1020 <lheesoo1020@gmail.com>
2025-04-08 14:39:57 +0900

    initial commit

---
+++ src/main/java/com/snowhite/saboteur/SaboteurApplication.java
@@ -0,0 +1,13 @@
+package com.snowhite.saboteur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaboteurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaboteurApplication.class, args);
+	}
+
+}
