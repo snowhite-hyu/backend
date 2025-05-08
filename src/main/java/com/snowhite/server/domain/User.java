d17014f933f5c799a4684b5690df5b156c51ed2a
Lee won-tae <dldnjsxo0524@hanyang.ac.kr>
2025-05-09 00:52:06 +0900

    fix : 경로 수정
    - ./src/main/java/com/snowhite/server/web/domain >
    - ./src/main/java/com/snowhite/server/domain

--- src/main/java/com/snowhite/server/domain/User.java
+++ src/main/java/com/snowhite/server/domain/User.java
@@ -1,8 +1,12 @@
-package com.snowhite.server.payload.domain;
+package com.snowhite.server.domain;
 
-import com.snowhite.server.payload.domain.common.BaseEntity;
+import com.snowhite.server.domain.common.BaseEntity;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "users")
 public class User extends BaseEntity {
