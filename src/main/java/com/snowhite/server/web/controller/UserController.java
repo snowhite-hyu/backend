3cfcbd5bfede649f0fe357c86a39e0cebd99cd18
je0ng3 <jepark2934@gmail.com>
2025-05-12 00:48:29 +0900

    refactor: api 경로 정리

--- src/main/java/com/snowhite/server/web/controller/UserController.java
+++ src/main/java/com/snowhite/server/web/controller/UserController.java
@@ -8,16 +8,14 @@ import com.snowhite.server.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/users")
 public class UserController {
 
     private final UserService userService;
