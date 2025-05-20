685627148ccad53046e608b18b70ecf991748515
lheesoo1020 <lheesoo1020@gmail.com>
2025-05-20 19:16:13 +0900

    fix: round start message payload 추가

--- src/main/java/com/snowhite/server/websocket/handler/GameWebSocketHandler.java
+++ src/main/java/com/snowhite/server/websocket/handler/GameWebSocketHandler.java
@@ -144,7 +144,7 @@ public class GameWebSocketHandler implements WebSocketHandler {
     public Mono<Void> handleStartRound(WebSocketSession session, Long gameId) {
 
         return gameService.setupGameForNewRound(gameId)
-                .flatMap(game -> broadcastMessageToGame(gameId, "Game-Started", null));
+                .flatMap(game -> broadcastMessageToGame(gameId, "Game-Started", game));
 
     }
 
