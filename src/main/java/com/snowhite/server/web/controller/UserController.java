a13341272e8224ed3e78c01807f86544c327d917
je0ng3 <jepark2934@gmail.com>
2025-05-21 21:28:38 +0900

    refactor: 회원관리 객체 리턴, 로그인 시 유저정보 유지

--- src/main/java/com/snowhite/server/web/controller/UserController.java
+++ src/main/java/com/snowhite/server/web/controller/UserController.java
@@ -2,9 +2,12 @@ package com.snowhite.server.web.controller;
 
 import com.snowhite.server.web.dto.web.request.LoginRequestDto;
 import com.snowhite.server.payload.ApiResponse;
-import com.snowhite.server.web.dto.EmailDto;
+import com.snowhite.server.web.dto.web.request.EmailDto;
 import com.snowhite.server.web.dto.web.request.RegisterDto;
 import com.snowhite.server.service.UserService;
+import com.snowhite.server.web.dto.web.response.EmailCheckResponseDto;
+import com.snowhite.server.web.dto.web.response.LoginResponseDto;
+import com.snowhite.server.web.dto.web.response.RegisterResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.server.reactive.ServerHttpRequest;
@@ -23,29 +26,53 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/login")
-    public Mono<ApiResponse<String>> login(@RequestBody LoginRequestDto loginRequestDto,
-                                           ServerWebExchange exchange) {
+    public Mono<ApiResponse<LoginResponseDto>> login(@RequestBody LoginRequestDto loginRequestDto,
+                                                     ServerWebExchange exchange) {
         ServerHttpResponse response = exchange.getResponse();
         return userService.login(loginRequestDto)
             .map(token -> {
                 response.getHeaders().add(HttpHeaders.AUTHORIZATION, "Bearer " + token);
-                return ApiResponse.onSuccess("access token: " + token);
+                return ApiResponse.onSuccess(
+                        LoginResponseDto
+                            .builder()
+                            .token(token)
+                            .build()
+                );
             });
     }
 
     @GetMapping("/check-login")
     public Mono<ApiResponse<String>> checkLogin(Authentication authentication, ServerWebExchange exchange) {
         ServerHttpRequest request = exchange.getRequest();
-        return userService.checkLogin(authentication, request);
+        return userService.checkLogin(authentication, request)
+                .map(ApiResponse::onSuccess);
     }
 
     @PostMapping("/register")
-    public Mono<ApiResponse<String>> register(@RequestBody RegisterDto registerDto) {
-        return userService.register(registerDto);
+    public Mono<ApiResponse<RegisterResponseDto>> register(@RequestBody RegisterDto registerDto) {
+        return userService.register(registerDto)
+                .map( isSuccess -> {
+                    String message = isSuccess ? "회원 가입에 성공하였습니다." : "회원 가입에 실패하였습니다. 이메일을 다시 확인해주세요.";
+                    return ApiResponse.onSuccess(
+                            RegisterResponseDto
+                                    .builder()
+                                    .isSuccess(isSuccess)
+                                    .message(message)
+                                    .build()
+                    );
+                });
     }
 
     @PostMapping("/check-email")
-    public Mono<ApiResponse<String>> checkEmail(@RequestBody EmailDto emailDto) {
-        return userService.checkEmail(emailDto);
+    public Mono<ApiResponse<EmailCheckResponseDto>> checkEmail(@RequestBody EmailDto emailDto) {
+        return userService.checkEmail(emailDto)
+            .map( isExisting -> {
+                String message = isExisting ? "사용 가능한 이메일입니다." : "사용 중인 이메일입니다.";
+                return ApiResponse.onSuccess(EmailCheckResponseDto
+                        .builder()
+                        .isExisting(isExisting)
+                        .message(message)
+                        .build());
+            });
     }
 }
