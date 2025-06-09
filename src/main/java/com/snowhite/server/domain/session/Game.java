b3624806b79085584cfe038bd8de3a5dc6b087a0
lheesoo1020 <lheesoo1020@gmail.com>
2025-06-10 08:27:14 +0900

    fix: row, column 혼동 수정

--- src/main/java/com/snowhite/server/domain/session/Game.java
+++ src/main/java/com/snowhite/server/domain/session/Game.java
@@ -279,8 +279,8 @@ public class Game {
         int goldRow = 1;
         int goldColumn = 8;
 
-        if (field[3][8][0] == 63) goldColumn = 3;
-        if (field[5][8][0] == 63) goldColumn = 5;
+        if (field[3][8][0] == 63) goldRow = 3;
+        if (field[5][8][0] == 63) goldRow = 5;
 
         return field[goldRow][goldColumn][3] == 1;
     }
